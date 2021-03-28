681a27d27e4c43704dd6c6359d27c10beb8a6b16
Amanda Campos <angels-fly-away@hotmail.com>
2021-03-27 23:49:16 -0400

    add dynamic GET route

--- server.js
+++ server.js
@@ -22,6 +22,17 @@ app.get('/users', function (req, res) {
     })
 })
 
+/*In Express, words with a colon in front of them in the url are treated as variables. You can access the value of each variable through req.params, like this*/
+app.get('/users/:id', function (req, res) {
+    console.log(req.params.id)
+    res.json({
+        success: true,
+        message: 'got one user',
+        user: req.params.id
+    })
+})
+
+
 /*The app.listen method will start up the server locally on the port you give as its first argument (in this case the base url is: http://localhost:8000)
  */
 app.listen(8000, function () {
