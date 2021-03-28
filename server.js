26f2d012455a4aa1fe6d025f30139b2f7b5ea086
Amanda Campos <angels-fly-away@hotmail.com>
2021-03-27 23:55:03 -0400

    add login POST route

--- server.js
+++ server.js
@@ -3,7 +3,10 @@
 const express = require('express');
 const app = express();
 
+/*So far, we have only made GET requests to our server. A POST request can send data securely through the request body. In order to make POST requests, first we need to include the "body-parser" library from our node_modules (included with express). Add these lines after the app variable: */
+const bodyParser = require('body-parser');
 
+app.use(bodyParser.json());
 
 
 const mockUserData = [
@@ -32,6 +35,28 @@ app.get('/users/:id', function (req, res) {
     })
 })
 
+
+//Let's write a function to handle a POST request made to the 'login' //endpoint, as if a user was trying to log in:
+app.post('/login', function (req, res) {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    const mockUsername = "billyTheKid";
+    const mockPassword = "superSecret";
+
+    if (username === mockUsername && password === mockPassword) {
+        res.json({
+            success: true,
+            message: 'password and username match!',
+            token: 'encrypted token goes here'
+        })
+    } else {
+        res.json({
+            success: false,
+            message: 'password and username do not match'
+        })
+    }
+})
 
 /*The app.listen method will start up the server locally on the port you give as its first argument (in this case the base url is: http://localhost:8000)
  */
